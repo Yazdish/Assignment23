94487c7b935e50dfa2aa21042174304d8e67cb51
Yazdan Shouryabi <yazdishouryabi@gmail.com>
2024-03-03 21:52:23 +0330

    Raw

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+from PySide6.QtWidgets import *
+from mainwindow import Ui_MainWindow
+from sudoku import Sudoku
+from functools import partial
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        puzzle = Sudoku(3).difficulty(0.5)
+        
+        self.line_edits = [[None for i in range(9)] for j in range(9)]
+        for i in range(9):
+            for j in range(9):
+                new_cell = QLineEdit()
+                if puzzle.board[i][j] != None:
+                    new_cell.setText(str(puzzle.board[i][j]))
+                    new_cell.setReadOnly(True)
+                self.ui.grid_layout.addWidget(new_cell, i, j)
+                new_cell.textChanged.connect(partial(self.validation, i, j))
+                self.line_edits[i][j] = new_cell
+
+    def validation(self, i, j, text):
+        if text not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            self.line_edits[i][j].setText("")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec_()
